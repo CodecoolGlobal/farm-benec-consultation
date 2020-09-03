e1b0cb261a8d6449fac3ecfe722de14ecdebb014
Bence Fabian <bence.fabian@codecool.com>
2020-09-03 17:29:11 +0200

    add shroomgrowers

--- impl/src/main/java/com/bencef/farm/plants/ShroomGrower.java
+++ impl/src/main/java/com/bencef/farm/plants/ShroomGrower.java
@@ -1,12 +1,41 @@
 package com.bencef.farm.plants;
 
+import java.util.Random;
+
 public class ShroomGrower extends Trait {
-    public ShroomGrower(Plant plant) {
+    private final int shroomGrowChance;
+    private final int productionIncrease;
+    private final int rotChance;
+    private final int productionDecrease;
+
+    private final Random random = new Random();
+
+    public ShroomGrower(Plant plant,
+                        int shroomGrowChance,
+                        int productionIncrease,
+                        int rotChance,
+                        int productionDecrease) {
         super(plant);
+        this.shroomGrowChance = shroomGrowChance;
+        this.productionIncrease = productionIncrease;
+        this.rotChance = rotChance;
+        this.productionDecrease = productionDecrease;
     }
 
     @Override
     public TraitResult apply() {
-        return TraitResult.ABORT;
+        if (chanceHappens(shroomGrowChance)) {
+            plant.monthlyProduce += productionIncrease;
+        }
+        if (chanceHappens(rotChance)) {
+            plant.monthlyProduce -= productionDecrease;
+            return TraitResult.ABORT;
+        }
+        return TraitResult.CONTINUE;
+    }
+
+    private boolean chanceHappens(int percent) {
+        int rn = random.nextInt(101);
+        return percent < rn;
     }
 }
--- impl/src/main/java/com/bencef/farm/plants/Spruce.java
+++ impl/src/main/java/com/bencef/farm/plants/Spruce.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Spruce extends Plant {
 
     public Spruce() {
-        var shroomGrower = new ShroomGrower(this);
+        var shroomGrower = new ShroomGrower(this, 5, 15, 4, 20);
         var everGreen = new EverGreen(this);
         traits.addAll(List.of(shroomGrower, everGreen));
     }
